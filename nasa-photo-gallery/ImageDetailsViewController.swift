b1608b68f20b4ca5021c8ea07d6838e82c0cc2a9
Shruti <15481881+sshruti20@users.noreply.github.com>
2025-12-29 23:07:09 +0530

    completed detail image screen

--- nasa-photo-gallery/ImageDetailsViewController.swift
+++ nasa-photo-gallery/ImageDetailsViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Shruti S on 29/12/25.
 //
 
+
+
 import UIKit
 
 class ImageDetailsViewController: UIViewController {
@@ -12,12 +14,51 @@ class ImageDetailsViewController: UIViewController {
     @IBOutlet weak var detailImageView: UIImageView!
     @IBOutlet weak var saveButton: UIButton!
     
+    var imageUrlString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        detailImageView.enableZoom()
+        
+        if imageUrlString != "" {
+            self.detailImageView.load(url: URL(string: imageUrlString)!)
+        }
     }
     
-
-
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        if let img = self.detailImageView.image {
+            UIImageWriteToSavedPhotosAlbum(img, self, #selector(saveCompleted), nil)
+        }
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+            if let error = error {
+                print("Save error: \(error.localizedDescription)")
+            } else {
+                print("Save finished!")
+                let alert = UIAlertController(title: "Success", message: "Image Saved!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    
 }
+
+
+extension UIImageView {
+  func enableZoom() {
+    let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(startZooming(_:)))
+    isUserInteractionEnabled = true
+    addGestureRecognizer(pinchGesture)
+  }
+
+  @objc
+  private func startZooming(_ sender: UIPinchGestureRecognizer) {
+    let scaleResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
+    guard let scale = scaleResult, scale.a > 1, scale.d > 1 else { return }
+    sender.view?.transform = scale
+    sender.scale = 1
+  }
+}
+
--- nasa-photo-gallery/ViewController.swift
+++ nasa-photo-gallery/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import Foundation
 
-var imgUrlString = ""
 
 class ViewController: UIViewController {
 
@@ -19,6 +18,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var copyrightLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var imgUrlString = ""
+
     
     var isScrollViewHidden = false {
         didSet {
@@ -120,9 +122,9 @@ class ViewController: UIViewController {
                     self.descriptionLabel.text = jsonObject?["explanation"] as? String
                     self.copyrightLabel.text = "Copyright" +  (jsonObject?["copyright"] as? String ?? "")
                 }
-                imgUrlString = jsonObject?["url"] as? String ?? ""
+                self.imgUrlString = jsonObject?["url"] as? String ?? ""
                 
-                self.imgView.load(url: (URL(string: imgUrlString) ?? URL(string: ""))!)
+                self.imgView.load(url: (URL(string: self.imgUrlString) ?? URL(string: ""))!)
             } catch {
                 print("JSON decoding error: \(error)")
             }
@@ -142,9 +144,9 @@ class ViewController: UIViewController {
                 self.descriptionLabel.text = "This is the mess that is left when a star explodes.  The Crab Nebula, the result of a supernova seen in 1054 AD, is filled with mysterious filaments.  The filaments are not only tremendously complex but appear to have less mass than expelled in the original supernova and a higher speed than expected from a free explosion.  The featured image was taken by an amateur astronomer in Leesburg, Florida, USA over three nights last month. It was captured in three primary colors but with extra detail provided by specific emission by hydrogen gas. The Crab Nebula spans about 10 light years.  In the Nebula's very center lies a pulsar: a neutron star as massive as the Sun but with only the size of a small town.  The Crab Pulsar rotates about 30 times each second.   Explore the Universe: Random APOD Generator"
                 self.copyrightLabel.text = "Copyright " +  "Alan Chen"
             }
-            imgUrlString = "https://apod.nasa.gov/apod/image/2512/Crab_Chen_960.jpg"
+        self.imgUrlString = "https://apod.nasa.gov/apod/image/2512/Crab_Chen_960.jpg"
         
-        self.imgView.load(url: (URL(string: imgUrlString) ?? URL(string: ""))!)
+        self.imgView.load(url: (URL(string: self.imgUrlString) ?? URL(string: ""))!)
         
     }
     
@@ -160,6 +162,15 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "Error", message: alertMsg, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "toImageDetailsVC" {
+                if let destinationVC = segue.destination as? ImageDetailsViewController {
+                    // Pass the data
+                    destinationVC.imageUrlString = self.imgUrlString
+                }
+            }
     }
     
 }
