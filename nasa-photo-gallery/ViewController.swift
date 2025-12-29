02deae20d0f747558a2468cee60c3457393a2adb
Shruti <15481881+sshruti20@users.noreply.github.com>
2025-12-29 15:05:29 +0530

    Initial Commit

---
+++ nasa-photo-gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nasa-photo-gallery
+//
+//  Created by Shruti S on 29/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
