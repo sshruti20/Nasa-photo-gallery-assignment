9d743f9b1a025fac95738e3c83d776128ec19183
Shruti <15481881+sshruti20@users.noreply.github.com>
2025-12-29 17:54:06 +0530

    setup up both basic viewcontrollers and their views

---
+++ nasa-photo-gallery/ImageDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageViewController.swift
+//  nasa-photo-gallery
+//
+//  Created by Shruti S on 29/12/25.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
