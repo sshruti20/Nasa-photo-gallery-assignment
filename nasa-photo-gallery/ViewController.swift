1a7d5cb00897c658b18dec9624561d2bedb66b57
Shruti <15481881+sshruti20@users.noreply.github.com>
2025-12-30 17:03:27 +0530

    slight change

--- nasa-photo-gallery/ViewController.swift
+++ nasa-photo-gallery/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     var imgUrlString = ""
 
     
-    var isScrollViewHidden = false {
+    var isScrollViewHidden = true {
         didSet {
             DispatchQueue.main.async {
                 self.scrollView.isHidden = self.isScrollViewHidden
@@ -143,6 +143,7 @@ class ViewController: UIViewController {
                 self.titleLabel.text = "M1: The Crab Nebula"
                 self.dateLabel.text = "2025-12-29"
                 self.descriptionLabel.text = "This is the mess that is left when a star explodes.  The Crab Nebula, the result of a supernova seen in 1054 AD, is filled with mysterious filaments.  The filaments are not only tremendously complex but appear to have less mass than expelled in the original supernova and a higher speed than expected from a free explosion.  The featured image was taken by an amateur astronomer in Leesburg, Florida, USA over three nights last month. It was captured in three primary colors but with extra detail provided by specific emission by hydrogen gas. The Crab Nebula spans about 10 light years.  In the Nebula's very center lies a pulsar: a neutron star as massive as the Sun but with only the size of a small town.  The Crab Pulsar rotates about 30 times each second.   Explore the Universe: Random APOD Generator"
+                //self.descriptionLabel.text! += self.descriptionLabel.text!
                 self.copyrightLabel.text = "Copyright " +  "Alan Chen"
             }
         self.imgUrlString = "https://apod.nasa.gov/apod/image/2512/Crab_Chen_960.jpg"
@@ -152,11 +153,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func loadImageButtonClicked(_ sender: Any) {
-        //self.isScrollViewHidden = false
+        self.isScrollViewHidden = false
         
-        getDataFromNasaApi()
-        //getSelectedDate()
-        //fillDataFromSampleResponse()
+        //getDataFromNasaApi()
+        getSelectedDate()
+        fillDataFromSampleResponse()
     }
     
     func showErrorAlert (alertMsg: String) {
@@ -173,6 +174,11 @@ class ViewController: UIViewController {
                 }
             }
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height+300)
+    }
+
     
 }
 
